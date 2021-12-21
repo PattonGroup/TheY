093153a7bd4e71cda241ea804de531c3cf7a5743
Ian Talisic <ianfreelancemail@gmail.com>
2021-12-21 23:58:26 +0800

    ccreated the screen for forgot password Added the view controller for it Added validations

---
+++ University Forum/Controllers/ForgotPassword.swift
@@ -0,0 +1,45 @@
+//
+//  ForgotPassword.swift
+//  University Forum
+//
+//  Created by Ian Talisic on 21/12/2021.
+//
+
+import UIKit
+import FirebaseAuth
+
+class ForgotPassword: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didTapSubmit(_ sender: Any) {
+        if emailTextField.text!.isEmpty {
+            SharedFunc.showError(title: "Error", errMsg: "Please enter your email address.")
+        }else if SharedFunc.isValidEmail(candidate: emailTextField.text!){
+            SharedFunc.showError(title: "Error", errMsg: "Please enter a valid email address.")
+        }else{
+            Auth.auth().sendPasswordReset(withEmail: emailTextField.text!) { error in
+                
+                if error != nil {
+                    SharedFunc.showSuccess(title: "Forgot Password", message: "Your teporary password has been sent to your email.")
+                }
+            }
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
